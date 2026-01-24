39c8ca9b2faadf88424342979ec3b729eee241d7
Brady Carden <bradyac21@gmail.com>
2026-01-24 08:43:18 -0600

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Brady Carden on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
